a29d8d544ef0954d955e422c814bf4c39be4dd5c
Lidne <play.gt.play@yandex.ru>
2021-02-05 16:45:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUi()
+
+    def initUi(self):
+        pass
